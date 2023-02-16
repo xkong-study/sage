ba1eb1848449170a154145292dfed2b01d687696
xkong-study <994885218@qq.com>
2023-02-16 17:53:01 +0000

    suggestion list with clock and heart icon

--- src/components/Suggestion.tsx
+++ src/components/Suggestion.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import {
+  ClockIcon,
+  PaperClipIcon,
+  HeartIcon,
+} from "@heroicons/react/24/outline";
 
 function Suggestion() {
-  return <div className="bg-black">Suggestion</div>;
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex space-x-2">
+        <ClockIcon className="h-12 w-12 text-blue-600 p-2 bg-blue-100 rounded-full" />
+        <div>
+          <dt className=" text-base">Flower CBD</dt>
+          <dl className=" text-sm text-gray-500">Capel Street,Rotunda</dl>
+        </div>
+      </div>
+      <span>
+      <HeartIcon className="h-12 w-12 text-pink-600 p-2 bg-pink-100 ring-1 ring-pink-600 rounded-full" />
+      </span>
+    </div>
+  );
 }
 
 export default Suggestion;
