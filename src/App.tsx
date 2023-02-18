0dc0dccd226758639a3e27e17543990bb59abc5c
xkong-study <994885218@qq.com>
2023-02-18 14:30:55 +0000

    implemented page5 from ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import SlidePanel from "./components/SlidePanel";
+import MyCombobox from "./components/page5";
 
 export default function App() {
   return (
     <div className="h-[100vh] w-[100vw] bg-black">
-      <SlidePanel />
+      <MyCombobox />
     </div>
   );
 }
